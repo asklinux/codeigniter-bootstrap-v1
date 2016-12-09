2272ead6b63f056a2c33219c132966f46ffc7c4e
root <hasnan@tunnelbiz.com>
2016-12-09 10:35:05 +0800

    pertama

---
+++ application/views/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Codineger Bootstrip Theme</title>
+<link rel="stylesheet" href="asset/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="asset/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="asset/css/main.css">
+
+		
+	</head>
+	
+<body>
+	
+	
